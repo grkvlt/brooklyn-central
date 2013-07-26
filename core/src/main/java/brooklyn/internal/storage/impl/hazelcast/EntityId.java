cfa50073e2f78114e1061b1fff47edf5992c97fd
Peter Veentjer <alarmnummer@gmail.com>
2013-07-26 13:54:44 +0300

    Added HazelcastDatagrid and added the StorageFactory

--- core/src/main/java/brooklyn/internal/storage/impl/hazelcast/EntityId.java
+++ core/src/main/java/brooklyn/internal/storage/impl/hazelcast/EntityId.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 
 class EntityId implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
     private final String id;
 
     EntityId(String id) {
