1b994e964cd232c0cbf7fb1faf2802e748024cdd
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2014-01-28 12:49:16 +0000

    Extracted readMemento into separate method and expanded mutex scope

--- core/src/main/java/brooklyn/entity/rebind/persister/BrooklynMementoPersisterToFile.java
+++ core/src/main/java/brooklyn/entity/rebind/persister/BrooklynMementoPersisterToFile.java
@@ -2,7 +2,6 @@ package brooklyn.entity.rebind.persister;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -10,33 +9,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import brooklyn.mementos.BrooklynMemento;
-import brooklyn.mementos.BrooklynMementoPersister.LookupContext;
+import brooklyn.util.exceptions.Exceptions;
 import brooklyn.util.time.Time;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Charsets;
-import com.google.common.base.Joiner;
 import com.google.common.base.Stopwatch;
-import com.google.common.base.Throwables;
 import com.google.common.io.Files;
 
 public class BrooklynMementoPersisterToFile extends AbstractBrooklynMementoPersister {
 
     // FIXME This is no longer used (instead we use ToMultiFile).
-    // Is this definitely no longer useful? Delete if not, and 
+    // Is this definitely no longer useful? Delete if not, and
     // merge AbstractBrooklynMementoPersister+BrooklynMementoPerisisterInMemory.
 
     private static final Logger LOG = LoggerFactory.getLogger(BrooklynMementoPersisterToFile.class);
 
     private final File file;
     private final MementoSerializer<BrooklynMemento> serializer;
-    private final Object mutex = new Object();
-    
+    private final Object mutex = new Object[0];
+
     public BrooklynMementoPersisterToFile(File file, ClassLoader classLoader) {
         this.file = file;
         this.serializer = new XmlMementoSerializer<BrooklynMemento>(classLoader);
     }
-    
+
     @VisibleForTesting
     @Override
     public void waitForWritesCompleted(long timeout, TimeUnit unit) throws InterruptedException, TimeoutException {
@@ -47,64 +44,64 @@ public class BrooklynMementoPersisterToFile extends AbstractBrooklynMementoPersi
 
     @Override
     public BrooklynMemento loadMemento(LookupContext lookupContext) {
-        try {
-            Stopwatch stopwatch = Stopwatch.createStarted();
-            List<String> lines;
-            synchronized (mutex) {
-                lines = Files.readLines(file, Charsets.UTF_8);
-            }
-            String xml = Joiner.on("\n").join(lines);
-            
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        synchronized (mutex) {
             serializer.setLookupContext(lookupContext);
             try {
-                BrooklynMemento result = serializer.fromString(xml);
-                
-                if (LOG.isDebugEnabled()) LOG.debug("Loaded memento; took {}", Time.makeTimeStringRounded(stopwatch.elapsed(TimeUnit.MILLISECONDS)));
+                BrooklynMemento result = readMemento();
+                if (LOG.isDebugEnabled()) LOG.debug("Loaded memento; took {}",
+                        Time.makeTimeStringRounded(stopwatch.elapsed(TimeUnit.MILLISECONDS)));
+
                 return result;
-                
             } finally {
                 serializer.unsetLookupContext();
             }
-            
-        } catch (IOException e) {
-            throw Throwables.propagate(e);
         }
     }
-    
+
     @Override
     public void checkpoint(BrooklynMemento newMemento) {
-        Stopwatch stopwatch = new Stopwatch().start();
+        Stopwatch stopwatch = Stopwatch.createStarted();
         synchronized (mutex) {
             long timeObtainedMutex = stopwatch.elapsed(TimeUnit.MILLISECONDS);
             super.checkpoint(newMemento);
             long timeCheckpointed = stopwatch.elapsed(TimeUnit.MILLISECONDS);
             writeMemento();
             long timeWritten = stopwatch.elapsed(TimeUnit.MILLISECONDS);
-            
+
             if (LOG.isDebugEnabled()) LOG.debug("Checkpointed memento; total={}ms, obtainingMutex={}ms, " +
-                    "checkpointing={}ms, writing={}ms", 
-                    new Object[] {timeWritten, timeObtainedMutex, (timeCheckpointed-timeObtainedMutex), 
+                    "checkpointing={}ms, writing={}ms",
+                    new Object[] {timeWritten, timeObtainedMutex, (timeCheckpointed-timeObtainedMutex),
                     (timeWritten-timeCheckpointed)});
         }
     }
-    
+
     @Override
     public void delta(Delta delta) {
-        Stopwatch stopwatch = new Stopwatch().start();
+        Stopwatch stopwatch = Stopwatch.createStarted();
         synchronized (mutex) {
             long timeObtainedMutex = stopwatch.elapsed(TimeUnit.MILLISECONDS);
             super.delta(delta);
             long timeDeltad = stopwatch.elapsed(TimeUnit.MILLISECONDS);
             writeMemento();
             long timeWritten = stopwatch.elapsed(TimeUnit.MILLISECONDS);
-            
+
             if (LOG.isDebugEnabled()) LOG.debug("Checkpointed memento; total={}ms, obtainingMutex={}ms, " +
-                    "delta'ing={}ms, writing={}", 
-                    new Object[] {timeWritten, timeObtainedMutex, (timeDeltad-timeObtainedMutex), 
+                    "delta'ing={}ms, writing={}",
+                    new Object[] {timeWritten, timeObtainedMutex, (timeDeltad-timeObtainedMutex),
                     (timeWritten-timeDeltad)});
         }
     }
-    
+
+    private BrooklynMemento readMemento() {
+        try {
+            return serializer.fromString(Files.asCharSource(file, Charsets.UTF_8).read());
+        } catch (IOException e) {
+            LOG.error("Failed to persist memento", e);
+            throw Exceptions.propagate(e);
+        }
+    }
+
     private void writeMemento() {
         try {
             Files.write(serializer.toString(memento), file, Charsets.UTF_8);
